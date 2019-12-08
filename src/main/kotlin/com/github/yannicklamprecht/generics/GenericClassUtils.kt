9ee3520a7b4a0edae510f7eba9ef29669978c832
ysl3000 <yannicklamprecht@live.de>
2019-12-08 16:35:41 +0100

    task: const Regex

--- src/main/kotlin/com/github/yannicklamprecht/generics/GenericClassUtils.kt
+++ src/main/kotlin/com/github/yannicklamprecht/generics/GenericClassUtils.kt
@@ -1,9 +1,15 @@
 package com.github.yannicklamprecht.generics
 
+
+object Pattern {
+    val REMOVE_ADDRESS = Regex("@\\w+$")
+    val REMOVE_PACKAGE = Regex("(\\w+\\.)+")
+}
+
 fun <T> T.getClassName(): String {
-    return this.toString().replace(Regex("@\\w+$"), "")
+    return this.toString().replace(Pattern.REMOVE_ADDRESS, "")
 }
 
 fun String.simpleName(): String {
-    return this.replace(Regex("(\\w+\\.)+"),"")
+    return this.replace(Pattern.REMOVE_PACKAGE, "")
 }
